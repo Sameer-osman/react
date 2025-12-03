58456cbe4657603cd9099d86e571839309dadf2d
Sameer-osman <sosman29@my.centennialcollege.ca>
2025-12-02 20:43:51 -0500

    Initialize React app with basic structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from React Demo!</h1>
+      <p>This React app is deployed on Netlify.</p>
+    </div>
+  );
+}
+
+export default App;
